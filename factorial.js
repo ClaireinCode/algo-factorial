064d1f7fa4dd1c4efd4e80fe58926b084948af7c
Claireese <claireese.underwood@gmail.com>
2023-10-04 16:47:29 -0500

    I gave up on the recursion and just used a for loop to find the factorial

--- factorial.js
+++ factorial.js
@@ -1,5 +1,20 @@
 function factorial(num) {
-  return 0;
+  // let newNumber = 1;
+  // function newFactorial(num){
+  // newNumber *= num;
+  //  if (num > 1){    
+  //    newFactorial(num - 1) 
+  //   }
+  // console.log(num + " " +newNumber)
+  // return newNumber;
+  // }
+  let newNumber = 1;
+  if (num > 1){
+    for (let i = num; i > 0; i--){
+      newNumber *= i;
+    }
+  }
+  //console.log(newNumber)
+  return newNumber;
 }
-
 module.exports = factorial;
